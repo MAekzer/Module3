238009f0d57346c99f21f2f2d1ebc56fb1cd9391
MAekzer <gma312@yandex.ru>
2022-07-23 18:07:12 +0300

    раздел 2

--- CApp1/Program.cs
+++ CApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, \n World!");
-Console.WriteLine("\x40");
-Console.WriteLine("\x23");
-Console.WriteLine(547567);
+string Name = "Jane";
+byte Age = 27;
+bool Answer = true;
+double Size = 37.5;
+
+Console.WriteLine("My name is {0}\nMy age is {1}\nDo I have a pet? {2}\nMy shoe size is {3}", Name, Age, Answer, Size);
+
