8b036c497276b7a5632a29945dc1a96f467b3ccd
MAekzer <gma312@yandex.ru>
2022-07-23 17:56:54 +0300

    Первый раздел

---
+++ CApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, \n World!");
+Console.WriteLine("\x40");
+Console.WriteLine("\x23");
+Console.WriteLine(547567);
--- CApp2/Program.cs
+++ CApp2/Program.cs
@@ -1,5 +1,5 @@
 ﻿string MyName;
 
-MyName = "Jane";
-Console.WriteLine(MyName);
+MyName = "Миша";
+Console.WriteLine("Привет!\n\tМеня зовут {0}.\n\nHello!\n\tMy name is Misha", MyName);
 Console.ReadKey();
